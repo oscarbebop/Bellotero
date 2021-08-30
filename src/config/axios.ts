db8ef2e32b71bb7e2f7e64a9b4026be1c460474e
Oscar <hello@castaneda.io>
2021-08-29 21:49:16 -0500

    baseurl updated

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const axiosCall = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://raw.githubusercontent.com/Bernabe-Felix/Bellotero/master',
 });
